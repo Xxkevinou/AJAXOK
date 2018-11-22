e3c7ce84e0aacbb730f9c0ad3d87c2159f4a7805
Kevinou <sophie-bretzel@laposte.net>
2018-11-22 17:04:10 +0100

    coucou

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,28 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require jquery3
+//= require popper
+//= require bootstrap-sprockets
 //= require rails-ujs
 //= require activestorage
 //= require turbolinks
 //= require_tree .
+
+$(document).ready(function(){
+    $(".tab_content").hide(); //Hide all content
+    //On Click Event
+    $(".mailer div").click(function() {
+        $(".mailer div").removeClass("active"); //Remove any "active" class
+        $(".tab_content").hide();
+        $(this).addClass("active"); //Add "active" class to selected tab
+        $(this).addClass("read");
+        $(".active .tab_content").show(); //Show tab content of active tab
+    });
+
+    $('.delete_email').click(function() {
+   		$(this).closest('.caca').fadeOut();
+
+	});
+
+});
--- db/seeds.rb
+++ db/seeds.rb
@@ -17,3 +17,18 @@ require 'faker'
     my_task.save
   end
 end
+
+10.times do |index|
+	email = Email.create!(object: "object#{index}", body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+		      											   Aliquam commodo augue mauris, quis volutpat augue eleifend 
+		      											   nec. Nunc consequat justo in convallis consectetur. In pharetra 
+		      											   magna quis placerat imperdiet. Morbi ac gravida ante. Praesent 
+		      											   porttitor quis diam vitae venenatis. Interdum et malesuada fames ac 
+		      											   ante ipsum primis in faucibus. Donec nisi eros, vestibulum vel luctus 
+		      											   posuere, iaculis nec elit. Sed eget ipsum justo. Ut dignissim eu elit 
+		      											   id commodo. Sed venenatis nibh mauris, vel cursus quam dapibus non. 
+		      											   Ut nec magna in ante tempor dictum. Nam euismod nec dui sed varius. Ut 
+		      											   nec rutrum turpis, non pretium ipsum. Nunc condimentum euismod dui, ac 
+		      											   porttitor quam placerat eget.")
+end
+
